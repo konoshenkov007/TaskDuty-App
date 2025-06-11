e30973dd82349ebf6b27fe25b773fbfab901f28f
v3nuchiha <v3nuchiha@protonmail.com>
2025-06-11 18:26:12 -0400

    Improved the New Task page

--- src/components/MyForm.jsx
+++ src/components/MyForm.jsx
@@ -3,55 +3,61 @@ import BackLogo from '../assets/images/back-icon.png'
 import { Link } from 'react-router-dom'
 import ScrollToTop from './ScrollToTop'
 
-const Form = ({ formTitle }) => {
+const Form = ({ formTitle, formData, onChange, onSubmit }) => {
   return (
     <>
 
-      <section className='items-center flex gap-4 pb-15'>
-        <Link to='/all-tasks' className='flex items-center gap-4'>
+      <section className='items-center flex gap-4 pb-10'>
+        <Link to='/all-tasks' className='flex items-center gap-4 py-2 md:py-4'>
           <img className='w-[15px] md:w-[18px] h-[26px] md:h-[36px]' src={BackLogo} alt='Back icon' />
-          <span className='fon-[400] text-[35px] md:text-[50px] text-[#292929]'>{formTitle}</span>
+          <span className='font-[400] text-[25px] md:text-[50px] text-[#292929]'>{formTitle}</span>
         </Link>
       </section>
 
       {/* FORM */}
       <section>
 
-        <form className='flex flex-col gap-11'>
+        <form onSubmit={onSubmit} className='flex flex-col gap-11'>
           <fieldset className='pb-5 pt-2 border-1 border-[#B8B6B6] px-10 rounded-[5px] w-full h-full'>
             <legend className='text-[#9C9C9C] font-[400] lg:text-[30px] text-[25px] '>Task Title</legend>
-              <input className='placeholder:font-[400] focus:outline-none focus:border-transparent placeholder-[#CCCCCC] text-[#292929] text-[22px] border-0 w-full' type='text' placeholder='E.g Project Defense, Assignment ...' />
+              <input onChange={onChange} name='taskTitle' value={formData.taskTitle} id='taskTitle' className='placeholder:font-[400] focus:outline-none focus:border-transparent placeholder-[#CCCCCC] text-[#292929] text-[22px] border-0 w-full' type='text' placeholder='E.g Project Defense, Assignment ...' />
           </fieldset>
 
           <fieldset className='pb-5 pt-2 border-1 border-[#B8B6B6] px-10 rounded-[5px] w-full h-full'>
             <legend className='text-[#9C9C9C] font-[400] lg:text-[30px] text-[25px]'>Description</legend>
-              <textarea className='placeholder:font-[400] focus:outline-none focus:border-transparent placeholder-[#CCCCCC] text-[#292929] text-[22px] border-0 w-full h-[262px]' type='text' placeholder='Briefly describe your task...'></textarea>
+              <textarea onChange={onChange} name='description' value={formData.description} id='description'  className='placeholder:font-[400] focus:outline-none focus:border-transparent placeholder-[#CCCCCC] text-[#292929] text-[22px] border-0 w-full h-[262px]' type='text' placeholder='Briefly describe your task...'></textarea>
           </fieldset>
+            
+            <div className="relative w-full h-full">
+              <fieldset className="pb-5 pt-2 border border-[#B8B6B6] px-10 rounded-[5px] w-full h-full">
+                <legend className="text-[#9C9C9C] font-[400] lg:text-[30px] text-[25px]">Tag</legend>
 
-          <div className='relative w-full h-full'>
-            <fieldset className='pb-5 pt-2 border border-[#B8B6B6] px-10 rounded-[5px] w-full h-full'>
-              <legend className='text-[#9C9C9C] font-[400] lg:text-[30px] text-[25px]'>Tag</legend>
+                {/* Fake Placeholder Tags */}
+                <div
+                  id="placeholder-tags"
+                  className="font-[400] text-[22px] absolute left-10 lg:top-[53px] top-[50px4 flex gap-2 pointer-events-none z-0"
+                >
+                  <span className="bg-[#9C9C9C] text-white px-2 py-1 rounded">Urgent</span>
+                  <span className="bg-[#9C9C9C] text-white px-2 py-1 rounded">Important</span>
+                </div>
 
-              {/* Fake Placeholder Tags */}
-              <div className='font-[400] text-[22px] absolute left-12 top-[55x] flex gap-2 pointer-events-none' id='placeholder-tags'>
-                <span className='bg-[#9C9C9C] text-white px-2  rounded'>Urgent</span>
-                <span className='bg-[#9C9C9C] text-white px-2 rounded'>Important</span>
-              </div>
+                {/* Select Field */}
+                <select
+                  id='tag'
+                  name="tag"
+                  value={formData.tag}
+                  onChange={onChange}
+                  className="bg-transparent text-black cursor-pointer px-2 py-1 rounded text-[22px] font-[400] appearance-none w-fit border-0 focus:outline-none focus:border-transparent"
+                >
+                  <option value="" disabled hidden></option>
+                  <option value="Urgent">Urgent</option>
+                  <option value="Important">Important</option>
+                </select>
+              </fieldset>
+            </div>
 
-              {/* Input */}
-              <input
-                type='text'
-                placeholder=''
-                className='placeholder:font-[400] focus:outline-none focus:border-transparent text-[#292929] text-[22px] border-0 w-full bg-transparent relative z-10'
-                onChange={(e) => {
-                  const tags = document.getElementById('placeholder-tags');
-                  if (tags) tags.style.display = e.target.value ? 'none' : 'flex';
-                }}
-              />
-            </fieldset>
 
-            <button className='mb-10 mt-20 w-full bg-[#974FD0] lg:h-[84px] h-[60px] rounded-[8px] font-[500] lg:text-[35px] text-[25px] text-[#FAF9FB]'>Done</button>
-          </div>
+            <button type='submit' className='mb-10 mt-20 w-full bg-[#974FD0] lg:h-[84px] h-[60px] rounded-[8px] font-[500] lg:text-[35px] text-[25px] text-[#FAF9FB]'>Done</button>
         </form>
         
         <ScrollToTop />
--- src/pages/NewTask.jsx
+++ src/pages/NewTask.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from '../components/MyForm'
 
 export const NewTask = () => {
+  const [formData, setFormData] = useState({
+    taskTitle:"",
+    description:"",
+    tag:""
+  })
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormData((prev) => ({...prev, [name]:value}));
+
+    if(name === 'tag'){
+      const tags = document.getElementById('placeholder-tags');
+      if (tags) tags.style.display = value ? 'none' : 'flex';
+      document.getElementById('tag').style.backgroundColor = "#9C9C9C";
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <>
       <main className='wrapper'>
-        <Form formTitle='New Task'/>
+        <Form formTitle='New Task' formData={formData} onChange={handleChange} onSubmit={handleSubmit}/>
       </main>
     </>
   )
